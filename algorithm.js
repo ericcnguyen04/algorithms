ba89a80b97a038e77f9efed0a8e00c30bd255eb5
ericcnguyen04 <ericc.win@gmail.com>
2023-02-18 19:51:47 -0500

    i finished the algorithm lecture

---
+++ algorithm.js
@@ -0,0 +1,28 @@
+// Main algorithms we are learning about
+// sorting and search algorithms
+
+/*
+Algorithms are evaluated based on their computational complexity, or the amount of resources it takes to run them. Those resources include:
+
+Time: The maximum amount of time it would take the algorithm to solve a problem.
+Space: The maximum amount of computer memory, or RAM, the algorithm needs to run.
+
+Is it very large or very small?
+Very structured or very unstructured?
+What types of data does it contain?
+*/
+
+const sortingAlgorithm = [
+    'bubble sort',
+    'insertion sort',
+    'merge sort',
+    'quick sort',
+    'bucket sort',
+    'radix sort'
+]
+
+const searchAlgorithm = [
+    'binary search',
+    'brute force search'
+]
+
---
+++ big-o-notation.js
@@ -0,0 +1,8 @@
+/*
+How is Big O notation used to describe algorithm
+
+define math stuff: constant, linear, quad, log, factoral
+
+analyze algorithms to determine big o runtime
+*/
+
