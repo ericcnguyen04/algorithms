74a51c2dc6f0104bded94a35733965c1d6c77f31
ericcnguyen04 <ericc.win@gmail.com>
2023-02-18 21:08:13 -0500

    algorithm studied!

--- algorithm.js
+++ algorithm.js
@@ -26,3 +26,41 @@ const searchAlgorithm = [
     'brute force search'
 ]
 
+const complexity = {
+    "constant complexity": ["great!",
+        'task will always take same amount of time',
+        'no matter the amount of input',
+        '0(1)'
+    ],
+    "logarithmic complexity": ["great!",
+        'cuts the problem in half each time',
+        'binary search',
+        '0(log(N))'
+    ],
+    "linear complexity": ['ok',
+        'as input increases, processing increases linearly',
+        'iow, one extra input = one more step for execution',
+        'this applies when searching an unsorted array',
+        '0(N)'
+    ],
+    "quadratic complexity": ['bad',
+        'a for loop within a for loop',
+        'O(N^2)'
+    ],
+    "factorial complexity": ['avoid at all costs',
+        'cringe',
+        '0(N!)'
+    ]
+}
+
+/*
+Big O is a popular technical interview subject because of just how fundamental it is to the way that we talk about algorithms. Interviewers want to make sure that you understand the difference between efficiency and inefficiency so that you aren’t writing code that takes excessive memory or energy to run.
+*/
+
+// Does the function have to go through an entire list? If so, there’s an N in that Big O class somewhere.
+
+// Are there nested loops? That might give you O(N^2) (or worse).
+
+// Does the function break the list into smaller chunks? You could have O(log(N)).
+
+// Is the amount of work the same, regardless of the size of the data set? You might have O(1).
--- big-o-notation.js
+++ big-o-notation.js
@@ -6,3 +6,15 @@ define math stuff: constant, linear, quad, log, factoral
 analyze algorithms to determine big o runtime
 */
 
+// Big O notation is used to describe relationship between two functions based on growth rate
+
+// is used to determine efficienty of an algorithm as input increases
+
+// time complexity -- amount of time it takes for an algorithm to run
+
+// space complexity -- amount of memory for algorithm to run
+
+const timeComplexity = [
+    'not measured in increments of time!!!',
+    'measure time based on number basic steps an algorithm needs to execute, relative to size of data'
+]
