6b7d681238e6bbce23f44e2a59697c6160d8d06f
ericcnguyen04 <ericc.win@gmail.com>
2023-02-18 21:39:31 -0500

    recursion is familiar

---
+++ recursion.js
@@ -0,0 +1,19 @@
+/*
+There are four basic steps to writing a recursive function:
+
+1. Define your function and parameters.
+2. Define your base case and return the computed result.
+3. Perform the action step.
+4. Return the function with new arguments to make progress toward the base case.
+*/
+
+/*
+Recursion operates by breaking down a problem into smaller chunks. So, if you have a problem that can be broken down in this way, recursion is a natural fit.
+*/
+
+/*
+Given how frequently it can be used to write algorithms and functions, recursion is a common interview topic. Here’s what you might encounter:
+
+Determining if a given problem is a good fit for a recursive solution and why.
+Writing a recursive function. Even if you’re not asked to code the solution, you might be asked to sketch or explain how the function would be written. Here’s what that might look like. 
+*/
