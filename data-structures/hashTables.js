bea9894f3572436ab310c637ab0b72e5c55c8a9d
ericcnguyen04 <ericc.win@gmail.com>
2023-02-20 11:52:38 -0500

    learning data structures

---
+++ data-structures/hashTables.js
@@ -0,0 +1,5 @@
+/*
+Define hash functions and hash tables.
+Articulate the benefits of hash tables, in particular as they relate to speed of data access.
+Define collisions and how to resolve them.ß
+*/
---
+++ data-structures/stacksAndQueue.js
@@ -0,0 +1,20 @@
+/* 
+Distinguish between the behavior of a stack and a queue.
+Determine situations in which you’d use a stack or queue versus another data structure.
+Build a stack and a queue using a linked list or an array.
+*/
+
+const stacks = [
+    'most recently added item, is the first to be removed'
+]
+
+const queues =[
+    'first-in, first-out',
+    'priority queue: contains a priority queue',
+    'double-end queue: performs at both ends'
+]
+
+const stacksAndQueues = [
+    'both can be implemented as an array or linked list'
+]
+
