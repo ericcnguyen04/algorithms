120816533e30ab2c60c4fd26a73d292851e7ab20
ericcnguyen04 <ericc.win@gmail.com>
2023-05-08 21:47:08 -0400

    returned

--- data-structures/hashTables.js
+++ data-structures/hashTables.js
@@ -1,5 +1,6 @@
 /*
 Define hash functions and hash tables.
 Articulate the benefits of hash tables, in particular as they relate to speed of data access.
-Define collisions and how to resolve them.ß
+Define collisions and how to resolve them.
 */
+
